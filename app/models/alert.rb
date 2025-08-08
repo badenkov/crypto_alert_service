848d33fcd77837aa6ee27c1780601bf86783b53e
Alexey Badenkov <alexey.badenkov@gmail.com>
2025-08-08 12:09:04 +0400

    Add validation to Alert

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,12 +1,18 @@
 class Alert < ApplicationRecord
   enum :direction, %w[ up down ]
 
-  # validates :symbol, pre
+  validates :symbol, presence: true, inclusion: { in: %w[ ETHUSDT BTCUSDT ] }
+  validates :threshold, presence: true, numericality: { greater_than: 0 }
+  validates :direction, presence: true
 
   after_commit :generate_price_threshold
 
   private
     def generate_price_threshold
       GeneratePriceThresholdJob.perform_later(self) if price_threshold.blank?
+    end
+
+    def accepted_symbols
+      %w[ ETHUSDT BTCUSDT ]
     end
 end
