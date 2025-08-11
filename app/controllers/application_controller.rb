1b04dc7e11f64e970ccd0a055b0e4fdb88972ddf
Alexey Badenkov <alexey.badenkov@gmail.com>
2025-08-11 12:13:45 +0400

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  if Rails.env.production?
+    http_basic_authenticate_with(
+      name: Rails.application.credentials[:basic_auth_user],
+      password: Rails.application.credentials[:basic_auth_password]
+    )
+  end
 end
